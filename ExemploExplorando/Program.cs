da2c867bc2ceb3d3a811739bc41ac81f3ac75d9a
Eliane Henriqueta <elianehenriqueta@gmail.com>
2022-10-17 16:25:48 -0300

    Modulo propriedades , metodos e construtores

---
+++ ExemploExplorando/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExemploExplorando.Models;
+
+
+
+Pessoa p1 = new Pessoa(nome:"eliane","Henriqueta",idade: 45);
+Pessoa p2= new Pessoa(nome:"agilson","Henriqueta",idade:60);
+Pessoa p3= new Pessoa(nome:"julia","Henriqueta",idade:10);
+
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.AdicionarAluno(p3);
+cursoDeIngles.ListarAlunos();
+
+
+
+
+
+/*Pessoa p1 = new Pessoa();
+p1.Nome = "eliane";
+p1.Sobrenome = "Henriqueta";
+p1.Idade = 30;
+p1.Apresentar();*/
